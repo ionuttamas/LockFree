ecf07e8c6e019ddf119ddeaabd9b13466feb0470
ionuttamas <tamash_ionut@hotmail.com>
2015-10-09 00:09:41 +0300

    Updated lock free queue implementation

--- LockFree/LockFree.Core/Queue/Queue.cs
+++ LockFree/LockFree.Core/Queue/Queue.cs
@@ -7,63 +7,88 @@ namespace LockFree.Core.Queue
         //Insert at tail and remove from head
         private Node<T> _head;
         private Node<T> _tail;
-
+        private readonly T _sentinel;
+        
         public Queue()
         {
-            _head = new Node<T>();
+            _head = null;
             _tail = _head;
         }
 
-        public Queue(T item)
+        public Queue(T sentinel) 
         {
-            _head = new Node<T>(item);
-            _tail = _head;
+            _head = new Node<T>(sentinel);
+            _tail = _head; 
+            _sentinel = sentinel;
         }
 
         public void Enqueue(T item)
         {
             Node<T> node = new Node<T>(item);
             Node<T> oldTail;
-            Node<T> oldNext;
+            Node<T> next;
 
             while (true)
             {
                 oldTail = _tail;
-                oldNext = oldTail.Next;
 
-                if (_tail== oldTail)
+                if (_tail == null && Atomic.CAS(ref _tail, null, node))
+                    break;
+                
+                next = oldTail.Next;
+
+                if (_tail == oldTail)
                 {
-                    if (oldNext == null)
+                    if (next == null)
                     {
                         if (Atomic.CAS(ref _tail.Next, null, node))
                             break;
                     }
                     else
                     {
-                        Atomic.CAS(ref _tail, oldTail, oldNext);
+                        Atomic.CAS(ref _tail, oldTail, next);
                     }
                 }
             }
-             
+
             Atomic.CAS(ref _tail, oldTail, node);
+            Atomic.CAS(ref _head.Next, null, node);
         }
 
         public T Dequeue()
         {
+            Node<T> head;
+            Node<T> oldHead;
+            Node<T> oldTail;
             Node<T> next;
-            Node<T> oldHead;  
 
-            do
+            while (true)
             {
-                oldHead = _head; 
-                next = _head.Next; 
+                oldTail = _tail;
+                head = _head.Next;
 
-                if (next == null)
-                    return _head.Value;
+                if (head == null)
+                    return _sentinel;
 
-            } while (!Atomic.CAS(ref _head, oldHead, next));
+                oldHead = head;
+                next = oldHead.Next;
 
-            return oldHead.Value;
+                if (head == oldHead)
+                {
+                    if (oldHead == oldTail)
+                    {
+                        Atomic.CAS(ref _tail, oldTail, next);
+
+                        if (next == null && Atomic.CAS(ref _head.Next, oldHead, null))
+                            return _sentinel; 
+                    }
+                    else
+                    {
+                        if (Atomic.CAS(ref _head.Next, oldHead, next))
+                            return oldHead.Value;
+                    }
+                }
+            }
         }
     }
 }
--- LockFree/LockFree.UnitTests/QueueTests.cs
+++ LockFree/LockFree.UnitTests/QueueTests.cs
@@ -12,11 +12,11 @@ namespace LockFree.UnitTests
     public class QueueTests
     {
         [TestCase(2, 100, 50)]
-        //[TestCase(1, 100, 60)]
-        //[TestCase(1, 1000, 500)]
-        //[TestCase(2, 10000, 5000)]
-        //[TestCase(2, 100000, 25000)]
-        //[TestCase(4, 150000, 85000)]
+        [TestCase(1, 100, 60)]
+        [TestCase(1, 1000, 500)]
+        [TestCase(2, 10000, 5000)]
+        [TestCase(2, 100000, 25000)]
+        [TestCase(4, 150000, 85000)]
         public void Queue_WithLowThreadCount_WorksCorrectly(int threads, int enqueues, int dequeues)
         {
             //The elements we insert are unique and sentinel value is -1
@@ -24,17 +24,7 @@ namespace LockFree.UnitTests
             int currentValue = initialValue;
             ConcurrentDictionary<int, int> table = new ConcurrentDictionary<int, int>();
             Core.Queue.Queue<int> queue = new Core.Queue.Queue<int>(initialValue);
-
-            queue.Enqueue(1);
-            queue.Enqueue(2);
-            queue.Enqueue(3);
-            queue.Dequeue();
-            queue.Dequeue();
-            queue.Dequeue();
-            queue.Enqueue(3);
-            queue.Enqueue(4);
-            queue.Dequeue();
-
+              
             ThreadBuilder
                 .Empty()
                 .AddThreads(() =>
@@ -42,8 +32,7 @@ namespace LockFree.UnitTests
                     for (int i = 0; i < enqueues; i++)
                     {
                         int value = Interlocked.Increment(ref currentValue);
-                        queue.Enqueue(value);
-                        Console.WriteLine("Enq "+value);
+                        queue.Enqueue(value); 
                     }
                 }, threads)
                 .AddThreads(() =>
@@ -52,23 +41,22 @@ namespace LockFree.UnitTests
                     {
                         int value = queue.Dequeue();
                         table.AddOrUpdate(value, x => 1, (k, v) => v + 1);
-                        Console.WriteLine("Deq " + value);
                     }
                 }, threads)
                 .Start();
 
-            //The sentinel value can be returned more than once if queue is empty at the time of a pop
-            int expectedPops = table.Keys.Count + (table.ContainsKey(initialValue) ? -1 : 0);
-            int actualPops = table.Count(x => x.Key != initialValue && x.Value == 1);
+            //The sentinel value can be returned more than once if queue is empty at the time of a dequeue
+            int expectedDequeues = table.Keys.Count + (table.ContainsKey(initialValue) ? -1 : 0);
+            int actualDequeues = table.Count(x => x.Key != initialValue && x.Value == 1);
           
-            Assert.AreEqual(expectedPops, actualPops);
+            Assert.AreEqual(expectedDequeues, actualDequeues);
         }
 
         [TestCase(10, 100000, 50000)]
         [TestCase(20, 100000, 50000)]
         [TestCase(20, 1000000, 250000)]
         [TestCase(40, 1500000, 850000)]
-        public void Stack_WithHighThreadCount_WorksCorrectly(int threads, int enqueues, int dequeues)
+        public void Queue_WithHighThreadCount_WorksCorrectly(int threads, int enqueues, int dequeues)
         {
             //The elements we insert are unique and sentinel value is -1
             int initialValue = -1;
